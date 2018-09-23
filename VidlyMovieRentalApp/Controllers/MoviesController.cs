2922f6d2fe8c0499ad0821e95ac44284d4ee2ebe
Asad-Mukhtar <asadazad99@gmail.com>
2018-09-23 09:31:37 -0700

    Add Movie Model And Random View and Controller

---
+++ VidlyMovieRentalApp/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMovieRentalApp.Models;
+
+namespace VidlyMovieRentalApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+            return View(movie);
+        }
+    }
+}
