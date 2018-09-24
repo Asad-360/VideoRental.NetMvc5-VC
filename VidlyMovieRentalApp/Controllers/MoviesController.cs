2479adf98217f053102c65fd975cabcf4c99d84a
Asad-Mukhtar <asadazad99@gmail.com>
2018-09-24 03:21:27 -0700

    Razor Syntax and Adding RandomViewModel

--- VidlyMovieRentalApp/Controllers/MoviesController.cs
+++ VidlyMovieRentalApp/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidlyMovieRentalApp.Models;
+using VidlyMovieRentalApp.ViewModels;
 
 namespace VidlyMovieRentalApp.Controllers
 {
@@ -12,21 +13,26 @@ namespace VidlyMovieRentalApp.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie()
+            var movie = new Movie
             {
                 Name = "Shrek!"
             };
-            //ViewData["Movie"]=movie
-            ViewBag.Movie = movie;
-            return View();
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Ted"},
+                new Customer() { Name = "Alex Bay"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            
+            return View(viewModel);
         }
-        //For Convention Based Routing use route.config
-        //Attribute based Routes
-        [Route("movies/released/{year:range(10,50):regex(\\d{2})}/{month}")]
-        public ActionResult ByReleasedDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+      
     }
 }
 
