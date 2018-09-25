9c8381fa5d9229bdaa801c2aae2e9f40c6f500bd
Asad-Mukhtar <asadazad99@gmail.com>
2018-09-25 06:48:35 -0700

    Exercise 01 Adding Customer Controller and Table view of it

---
+++ VidlyMovieRentalApp/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMovieRentalApp.Models;
+
+namespace VidlyMovieRentalApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Marry Williams", Id = 1},
+                new Customer {Name = "John Doe" , Id=2}
+            };
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
