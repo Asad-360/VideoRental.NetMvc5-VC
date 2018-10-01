74342435baee5d266beb5b3600acc4339fdf4109
Asad-Mukhtar <asadazad99@gmail.com>
2018-09-30 23:34:00 -0700

    Making CustomerDto

---
+++ VidlyMovieRentalApp/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VidlyMovieRentalApp.Models;
+
+namespace VidlyMovieRentalApp.Dtos
+{
+    public class CustomerDto
+    {
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Customer Name")]
+        [StringLength(255)]
+
+        public String Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+
+        public byte MembershipTypeId { get; set; }
+
+    }
+}
